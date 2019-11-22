551ee806b8114d545b5c7e6045bead709c7a9382
zouchanglin <zchanglin@aliyun.com>
2019-11-22 10:27:09 +0800

    init

---
+++ src/main/java/edu/xpu/journey/JourneyApplication.java
@@ -0,0 +1,13 @@
+package edu.xpu.journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/xpu/journey/JourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xpu.journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
